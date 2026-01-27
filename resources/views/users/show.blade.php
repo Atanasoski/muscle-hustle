4d54bcf9dc7acc72d7b38f8cc3375fa4cac8be13
Kiril Atanasoski <atanasoski992@gmail.com>
2026-01-27 22:18:36 +0100

    standardize breadcrumbs

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,25 +5,7 @@
 @section('content')
 <div class="p-6 w-full max-w-full overflow-x-hidden">
     <!-- Breadcrumb -->
-    <div class="mb-4">
-        <nav class="flex" aria-label="Breadcrumb">
-            <ol class="inline-flex items-center space-x-1 md:space-x-3">
-                <li class="inline-flex items-center">
-                    <a href="{{ route('users.index') }}" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                        Users
-                    </a>
-                </li>
-                <li>
-                    <div class="flex items-center">
-                        <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                        </svg>
-                        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">{{ $user->name }}</span>
-                    </div>
-                </li>
-            </ol>
-        </nav>
-    </div>
+    <x-common.page-breadcrumb :pageTitle="$user->name" :items="[['label' => 'Users', 'url' => route('users.index')]]" />
 
     <!-- Header -->
     <div class="mb-6">
