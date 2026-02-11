1ba17a69e99e086308e9e54638db5cc342c7fc97
Kiril Atanasoski <atanasoski992@gmail.com>
2026-02-11 23:55:23 +0100

    ix ExerciseSeeder category key

--- database/seeders/ExerciseSeeder.php
+++ database/seeders/ExerciseSeeder.php
@@ -1500,7 +1500,7 @@ class ExerciseSeeder extends Seeder
                     'name' => $exerciseData['name'],
                 ],
                 [
-                    'category_id' => $categories[$exerciseData['category']] ?? null,
+                    'category_id' => isset($exerciseData['category']) ? ($categories[$exerciseData['category']] ?? null) : null,
                     'default_rest_sec' => $exerciseData['default_rest_sec'],
                     'muscle_group_image' => $muscleGroupImagePath,
                 ]
